9b50db78f568b0c38479ae7f064ba18f04a47a34
matthias@klein0r.de <Matthias Kleine>
2019-05-14 21:45:24 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'youtube adapter settings': {
+        'en': 'Adapter settings for youtube',
+        'de': 'Adaptereinstellungen für youtube',
+        'ru': 'Настройки адаптера для youtube',
+        'pt': 'Configurações do adaptador para youtube',
+        'nl': 'Adapterinstellingen voor youtube',
+        'fr': "Paramètres d'adaptateur pour youtube",
+        'it': "Impostazioni dell'adattatore per youtube",
+        'es': 'Ajustes del adaptador para youtube',
+        'pl': 'Ustawienia adaptera dla youtube',
+        'zh-cn': 'youtube的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'option1',
+        'ru': 'option1',
+        'pt': 'option1',
+        'nl': 'option1',
+        'fr': 'option1',
+        'it': 'option1',
+        'es': 'option1',
+        'pl': 'option1',
+        'zh-cn': 'option1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'option2',
+        'ru': 'option2',
+        'pt': 'option2',
+        'nl': 'option2',
+        'fr': 'option2',
+        'it': 'option2',
+        'es': 'option2',
+        'pl': 'option2',
+        'zh-cn': 'option2'
+    }
+};
